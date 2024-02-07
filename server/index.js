475714cbdbc919ac2213ee5ba879ccfa3c65b567
RewSuphakit <rewkisuphakit44@gmail.com>
2024-02-07 21:33:11 +0700

    Express+Prisma

--- server/index.js
+++ server/index.js
@@ -5,14 +5,12 @@ require("dotenv").config();
 const { readdirSync } = require('fs');
 const morgan = require('morgan');
 const cors = require('cors');
-
 const prisma = require('./models/db');
 const authRoutes = require('./Routers/authRoutes');
 const authenticate = require('./middleware/authenticate');
 const productRoutes = require('./Routers/productRouter'); // Import productRouter
 const addressRoutes = require('./Routers/addressRoutes')
 const port = process.env.PORT;
-
 app.use(morgan('dev'));
 app.use(cors());
 app.use(express.json());
@@ -23,7 +21,7 @@ app.use((req, res, next) => {
   next();
 });
 
-// Use authenticate middleware for routes that require authentication
+
 app.use('/auth', authRoutes);
 app.use('/address',addressRoutes)
 // api/product
