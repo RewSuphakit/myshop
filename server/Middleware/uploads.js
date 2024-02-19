b70c51b573db1502f8d8557a7d0dde4251b28131
RewSuphakit <rewkisuphakit44@gmail.com>
2024-02-19 19:02:04 +0700

    Changes

--- server/Middleware/uploads.js
+++ server/Middleware/uploads.js
@@ -1,19 +1,16 @@
 const multer = require('multer');
 const sharp = require('sharp');
 
-// กำหนดตำแหน่งที่จะเก็บไฟล์ที่อัปโหลด
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, 'uploads/');
   },
   filename: function (req, file, cb) {
-    // สร้างชื่อไฟล์ใหม่โดยเพิ่มเวลาปัจจุบันลงไปเป็นส่วนของชื่อไฟล์
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
     cb(null, uniqueSuffix + '-' + file.originalname);
   }
 });
 
-// กำหนดชนิดของไฟล์ที่ยอมรับ
 const fileFilter = (req, file, cb) => {
   if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
     cb(null, true);
@@ -22,7 +19,6 @@ const fileFilter = (req, file, cb) => {
   }
 };
 
-// กำหนดตัวแปร upload เป็น multer middleware
 const upload = multer({ 
   storage: storage, 
   fileFilter: fileFilter 
